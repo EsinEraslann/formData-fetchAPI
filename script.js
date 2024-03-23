1d6c7eeb4880a4987dac94b5b0544a41a4d8b7dc
EsinEraslann <esineraslanx@gmail.com>
2024-03-23 15:23:30 +0300

    validation

--- script.js
+++ script.js
@@ -29,18 +29,61 @@
     });
 }); */
 
+
+
+document.getElementById('firstname').addEventListener('input', function() {
+    if (!isValidName(this.value)) {
+        this.value = this.value.replace(/[^A-Za-zğüşıöçĞÜŞİÖÇ]+/g, '');
+    }
+});
+
+document.getElementById('lastname').addEventListener('input', function() {
+    if (!isValidName(this.value)) {
+        this.value = this.value.replace(/[^A-Za-zğüşıöçĞÜŞİÖÇ]+/g, '');
+    }
+});
+
+document.getElementById('phone').addEventListener('input', function() {
+    if (!isValidPhoneNumber(this.value)) {
+        this.value = this.value.replace(/\D/g, '');
+    }
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const passwordInput = document.getElementById('password');
+    const password = passwordInput.value;
+
+    if (password.length < 8 || password.length > 15) {
+        document.getElementById('error-message').innerText = 'Şifre 8 ile 15 karakter arasında olmalıdır.';
+        passwordInput.style.borderColor = 'red';
+        return;
+    }
+    
+    this.submit();
+});
+
+
+function isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return emailRegex.test(email);
+}
+
+
+// FORM SUBMİT
 document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Formdaki verileri al
+    // Formdaki verileri alıyoruz
     const formData = new FormData(this);
-    
-    // FormData nesnesini JavaScript nesnesine dönüştür
+
+    // FormData nesnesini JavaScript nesnesine dönüştürüyoruz
     const data = {};
     formData.forEach(function(value, key){
         data[key] = value;
     });
-    
+
     // JSON olarak veriyi hazırla ve API'ye POST isteği gönder
     fetch('https://reqres.in/api/users', {
         method: 'POST',
@@ -57,4 +100,16 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
         console.error('Hesap oluşturma hatası:', error);
     });
 });
+
+
+// doğrulama kısmı
+function isValidName(name) {
+    return /^[A-Za-zğüşıöçĞÜŞİÖÇ]+$/.test(name);
+}
+
+function isValidPhoneNumber(phoneNumber) {
+    return /^\d+$/.test(phoneNumber);
+}
+
+
 
